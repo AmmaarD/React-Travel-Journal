94e57c07c08e0ff0fa3bcf126d28b80ad8ef2ded
Ammaar Doctor <ammaar.doctor@gmail.com>
2025-01-03 17:08:55 -0600

    Journal Entry component complete using props

--- Travel-Journal/src/components/Header.jsx
+++ Travel-Journal/src/components/Header.jsx
@@ -1,9 +1,10 @@
-import imgURL from "../Globe.png"
+import imgURL from "../images/globe.png";
+
 export default function Header() {
   return (
-    <header>
-      <img src={imgURL} alt="globe" />
-      <h1>Header</h1>
-    </header>
+      <header>
+          <img src={imgURL} />
+          <h1>my travel journal.</h1>
+      </header>
   )
 }
